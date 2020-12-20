72fe945fb4cffc1282ee91cc0dd9b573e444debf
Jyotiprakash Mohanta <jpmohanta32@gmail.com>
2020-12-20 21:27:43 +0530

    add home screen

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -3,13 +3,17 @@ import { View,Text,ImageBackground,Pressablessa, Pressable } from 'react-native'
 
 import styles from './styles'
 
+import Fontisto from 'react-native-vector-icons/Fontisto'
 const HomeScreen =(props) => {
     return(
         <View>
             <Pressable 
-            style={styles.button}
-            onPress={()=> console.warn("Explore button clicked")}>
-                <Text style={styles.buttonText}>W</Text>
+            style={styles.searchButton}
+            onPress={()=> console.warn("search button clicked")}>
+                <Fontisto name={"search"} size={25} color={"#f15454"}/>
+                <Text style={styles.searchButtonText}>
+                    Where are you going?
+                </Text>
             </Pressable>
             
 
--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet,Dimensions } from 'react-native'
 
 const styles = StyleSheet.create({
     image:{
@@ -18,6 +18,7 @@ const styles = StyleSheet.create({
         backgroundColor:'#fff',
         width:200,
         height:30,
+        marginTop:25,
         marginLeft:25,
         borderRadius:10,
         justifyContent:'center',
@@ -27,7 +28,23 @@ const styles = StyleSheet.create({
     buttonText:{
         fontSize:16,
         fontWeight:'bold'
+    },
+    searchButton:{
+        position:'absolute',
+        top:20,
+        height:60,
+        width:Dimensions.get('screen').width - 20,
+        zIndex:2,
+        borderRadius:30,
+        marginHorizontal:10,
+        backgroundColor:'#fff',
+        flexDirection:'row',
+        justifyContent:'center',
+        alignItems:'center',
+        elevation: (Platform.OS === 'android') ? 50 : 0
+
     }
+    
 
 });
 
