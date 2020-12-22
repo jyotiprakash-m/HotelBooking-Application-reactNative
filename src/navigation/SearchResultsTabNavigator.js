48fff7c3943a7107bbed5aca51e8319bb59f4222
Jyotiprakash Mohanta <jpmohanta32@gmail.com>
2020-12-22 09:51:22 +0530

    Top tab bar added

---
+++ src/navigation/SearchResultsTabNavigator.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs'
+
+import SearchResultsScreen from '../screens/SearchResults';
+
+const Tab = createMaterialTopTabNavigator();
+
+export default function SearchResultsTabNavigator() {
+    return (
+        <Tab.Navigator tabBarOptions={{
+            activeTintColor:'#f15454',
+            indicatorStyle:{
+                backgroundColor:'#f15454'
+            }
+        }}>
+            <Tab.Screen name={"List"} component={SearchResultsScreen}/>
+            <Tab.Screen name={"Map"} component={SearchResultsScreen}/>
+        </Tab.Navigator>
+        
+    )
+}
